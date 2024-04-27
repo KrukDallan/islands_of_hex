3ff83ff8d8aa6b7b6d1fd7e10ef137a32a725b8e
KrukDallan <99502048+KrukDallan@users.noreply.github.com>
2024-04-27 18:19:41 +0200

    small edit

--- src/gui.rs
+++ src/gui.rs
@@ -1,15 +1,15 @@
 use eframe::egui;
 use egui::{pos2, Pos2};
 
-pub fn hexagon_vertices(start: egui::Pos2, distance: f32) -> Vec<Pos2> {
+pub fn hexagon_vertices(center: egui::Pos2, distance: f32) -> Vec<Pos2> {
     let mut vertices: Vec<Pos2> = Vec::new();
     //vertices.push(start);
     let angle_deg: f64 = 60.0; // Angle between two consecutive vertices in degrees
 
     for i in 0..6 {
         let angle_rad = angle_deg.to_radians() * i as f64;
-        let x = start[0] + distance * angle_rad.cos() as f32;
-        let y = start[1] + distance * angle_rad.sin() as f32;
+        let x = center[0] + distance * angle_rad.cos() as f32;
+        let y = center[1] + distance * angle_rad.sin() as f32;
         vertices.push(pos2(x, y));
     }
 
