68ede5513369dbc7ad18b4809dc5ca9e37e4c3a6
KrukDallan <99502048+KrukDallan@users.noreply.github.com>
2024-04-27 18:18:55 +0200

    small edit

--- src/gui.rs
+++ src/gui.rs
@@ -9,20 +9,24 @@ pub fn hexagon_vertices(start: egui::Pos2, distance: f32) -> Vec<Pos2> {
     for i in 0..6 {
         let angle_rad = angle_deg.to_radians() * i as f64;
         let x = start[0] + distance * angle_rad.cos() as f32;
-        let y = start[1]+ distance * angle_rad.sin() as f32;
+        let y = start[1] + distance * angle_rad.sin() as f32;
         vertices.push(pos2(x, y));
     }
 
     vertices
 }
 
+/* fn thrash_bin() {
+    let desired_size = ui.spacing().interact_size.y * egui::vec2(2.0, 2.0);
+    let (mut rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
 
-fn thrash_bin() {
-    /*             let desired_size = ui.spacing().interact_size.y * egui::vec2(2.0, 2.0);
-            let (mut rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
-
-            rect = rect.translate(vec2(100.0, 100.0));
-            let square = egui::Shape::rect_stroke(rect, egui::Rounding::default(), (10.0, egui::Color32::WHITE));
-            let mut point = egui::pos2(10.0, 10.0);
-            let line = egui::Shape::line_segment([1.0*point, 100.0*point ], (10.0,egui::Color32::WHITE)); */
-}
+    rect = rect.translate(vec2(100.0, 100.0));
+    let square = egui::Shape::rect_stroke(
+        rect,
+        egui::Rounding::default(),
+        (10.0, egui::Color32::WHITE),
+    );
+    let mut point = egui::pos2(10.0, 10.0);
+    let line =
+        egui::Shape::line_segment([1.0 * point, 100.0 * point], (10.0, egui::Color32::WHITE));
+} */
